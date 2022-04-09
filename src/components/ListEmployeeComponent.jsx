d502cf611467ff657b9ce3538a64f6e19988ce24
Per T <thorander@hotmail.com>
2022-04-09 23:34:31 +0200

    first commit

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService'
+
+class ListEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                employees: []
+        }
+        this.addEmployee = this.addEmployee.bind(this);
+        this.editEmployee = this.editEmployee.bind(this);
+        this.deleteEmployee = this.deleteEmployee.bind(this);
+    }
+
+    deleteEmployee(id){
+        EmployeeService.deleteEmployee(id).then( res => {
+            this.setState({employees: this.state.employees.filter(employee => employee.id !== id)});
+        });
+    }
+    viewEmployee(id){
+        this.props.history.push(`/view-employee/${id}`);
+    }
+    editEmployee(id){
+        this.props.history.push(`/add-employee/${id}`);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployees().then((res) => {
+            this.setState({ employees: res.data});
+        });
+    }
+
+    addEmployee(){
+        this.props.history.push('/add-employee/_add');
+    }
+
+    render() {
+        return (
+            <div>
+                 <h3 className="text-center">Teacher List</h3>
+                 <div className = "row">
+                    <button className="btn btn-primary" onClick={this.addEmployee}> Add Teacher</button>
+                 </div>
+                 <br></br>
+                 <div className = "row">
+                        <table className = "table table-striped table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th> ID</th>
+                                    <th> First Name</th>
+                                    <th> Last Name</th>
+                                    <th> Email </th>
+                                    <th> Role</th>
+                                    <th> Bank Account</th>
+                                    <th> Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.employees.map(
+                                        employee => 
+                                        <tr key = {employee.id}>
+                                            <td> { employee.id} </td>   
+                                             <td> { employee.firstName} </td>   
+                                             <td> {employee.lastName}</td>
+                                             <td> {employee.email}</td>
+                                             <td> {employee.role}</td>
+                                             <td> {employee.bankAccount}</td>
+                                             <td>
+                                                 <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info">Update </button>
+                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete </button>
+                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info">View </button>
+                                             </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
+                 </div>
+
+            </div>
+        )
+    }
+}
+
+export default ListEmployeeComponent
---
+++ src/components/UpdateEmployeeComponent.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from 'react'
+import EmployeeService from '../services/EmployeeService';
+
+class UpdateEmployeeComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            email: '',
+            role:'',
+            bankAccount:''
+        }
+        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.updateEmployee = this.updateEmployee.bind(this);
+    }
+
+    componentDidMount(){
+        EmployeeService.getEmployeeById(this.state.id).then( (res) =>{
+            let employee = res.data;
+            this.setState({firstName: employee.firstName,
+                lastName: employee.lastName,
+                email: employee.email,
+                role: employee.role, 
+                bankAccount: employee.bankAccount
+            });
+        });
+    }
+
+    updateEmployee = (e) => {
+        e.preventDefault();
+        let employee = {firstName: this.state.firstName, lastName: this.state.lastName, email: this.state.email, role: this.state.role, bankAccount: this.state.bankAccount};
+        console.log('employee => ' + JSON.stringify(employee));
+        console.log('id => ' + JSON.stringify(this.state.id));
+        EmployeeService.updateEmployee(employee, this.state.id).then( res => {
+            this.props.history.push('/employees');
+        });
+    }
+    
+    changeFirstNameHandler= (event) => {
+        this.setState({firstName: event.target.value});
+    }
+
+    changeLastNameHandler= (event) => {
+        this.setState({lastName: event.target.value});
+    }
+
+    changeEmailHandler= (event) => {
+        this.setState({email: event.target.value});
+    }
+
+    changeRoleHandler= (event) => {
+        this.setState({role: event.target.value});
+    }
+
+    changeBankAccountHandler= (event) => {
+        this.setState({bankAccount: event.target.value});
+    }
+
+    cancel(){
+        this.props.history.push('/employees');
+    }
+
+    render() {
+        return (
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3">
+                                <h4 className="text-center">Update Employee</h4>
+                                <div className = "card-body">
+                                    <form>
+                                        <div className = "form-group">
+                                            <label> First Name: </label>
+                                            <input placeholder="First Name" name="firstName" className="form-control" 
+                                                value={this.state.firstName} onChange={this.changeFirstNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Last Name: </label>
+                                            <input placeholder="Last Name" name="lastName" className="form-control" 
+                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Email: </label>
+                                            <input placeholder="Email Address" name="emailId" className="form-control" 
+                                                value={this.state.email} onChange={this.changeEmailHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Role: </label>
+                                            <input placeholder="Role" name="role" className="form-control" 
+                                                value={this.state.role} onChange={this.changeRoleHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Bank Account: </label>
+                                            <input placeholder="bankAccount" name="bankAccount" className="form-control" 
+                                                value={this.state.bankAccount} onChange={this.changeBankAccountHandler}/>
+                                        </div>
+
+                                        <button className="btn btn-success" onClick={this.updateEmployee}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+        )
+    }
+}
+
+export default UpdateEmployeeComponent
